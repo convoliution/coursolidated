5390406832f2e48ad64e6cb48088488d9eb2d58e
Michael Liu <miliu@protonmail.com>
2018-02-06 10:07:15 -0800

    animate side menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+// menu animation
 $(function() {
-
+    var menuWidth = '16rem';
+    var menuAnimTime = 250;
+    $('#topbar > button').prop('visible', false);
+    $('#topbar > button').tap(function() {
+        if (!$('#topbar > button').prop('visible')) {
+            $('#menu').animate({
+                width: menuWidth
+            }, menuAnimTime);
+            $('main').animate({
+                marginLeft: menuWidth
+            }, menuAnimTime);
+            $('#topbar > button').prop('visible', true);
+        } else {
+            $('#menu').animate({
+                width: 0
+            }, menuAnimTime);
+            $('main').animate({
+                marginLeft: 0
+            }, menuAnimTime);
+            $('#topbar > button').prop('visible', false);
+        }
+    });
 });
