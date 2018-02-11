ae19f1d1ed48a837d0cce013edcb110e9bea685f
Michael Liu <miliu@protonmail.com>
2018-02-10 21:12:46 -0800

    revamp sliding menu

--- public/js/menus.js
+++ public/js/menus.js
@@ -6,14 +6,13 @@ $(function() {
 // menu animation
 $(function() {
     let animTime = 250;
+    $('#topbar > button').tap(function(event) {
+        showMenu('#main-menu', animTime);
+    });
     $('.menu-top > button').tap(function(event) {
         event.preventDefault();
         let menuID = "#" + $(this).parent().parent().attr('id');
-        if (!$(menuID).prop('visible')) {
-            showMenu(menuID, animTime);
-        } else {
-            hideMenu(menuID, animTime);
-        }
+        hideMenu(menuID, animTime);
     });
     $('#main-menu > .menu-content > button').tap(function(event) {
         event.preventDefault();
@@ -24,12 +23,8 @@ $(function() {
 function showMenu(menuID, animTime) {
     if (menuID === '#main-menu') {
         $('main').animate({
-            marginLeft: $(menuID).width()
+            marginLeft: $(menuID).outerWidth()
         }, animTime);
-        $('#main-menu > .menu-content').animate({
-            width: $(menuID).width()
-        }, animTime);
-        $('#main-menu > .menu-top > button').text('<');
     }
     $(menuID).animate({
         left: 0
@@ -39,19 +34,11 @@ function showMenu(menuID, animTime) {
 function hideMenu(menuID, animTime) {
     if (menuID === '#main-menu') {
         $('main').animate({
-            marginLeft: $('#topbar').outerHeight()
-        }, animTime);
-        $('#main-menu').animate({
-            left: -$(menuID).width() + $('#topbar').outerHeight()
-        }, animTime);
-        $('#main-menu > .menu-content').animate({
-            width: $(menuID).width() - $('#topbar').outerHeight()
-        }, animTime);
-        $('#main-menu > .menu-top > button').text('>');
-    } else {
-        $(menuID).animate({
-            left: -$(menuID).width()
+            marginLeft: 0
         }, animTime);
     }
+    $(menuID).animate({
+        left: -$(menuID).outerWidth()
+    }, animTime);
     $(menuID).prop('visible', false);
 }
