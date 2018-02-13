e20ec153a79354ad889487caef0605afa21f52b0
Michael Liu <miliu@protonmail.com>
2018-02-12 18:29:27 -0800

    dynamically populate schedule with user's courses

--- app.js
+++ app.js
@@ -3,22 +3,45 @@
  * Module dependencies.
  */
 
-var express = require('express');
 var http = require('http');
 var path = require('path');
-var handlebars = require('express3-handlebars')
-
-var index = require('./routes/index');
-// Example route
-// var user = require('./routes/user');
+var express = require('express');
+var expresshbs = require('express3-handlebars')
 
 var app = express();
+
+var handlebars = expresshbs.create({
+    helpers: {
+        populateTerm: function(termKey, courses) {
+            var termLabel = {
+                'f': "fall",
+				'w': "winter",
+				's': "spring",
+				's1': "summer1",
+                's2': "summer2"
+            }[termKey]
+            ret = "<div class=\"term-label\">" + termLabel + "</div>\n"
+                + "<div class=\"term " + termLabel + "\" id=\"{{@index}}-" + termLabel + "\">\n"
+            var coursesCount = 6;
+            for (let course of courses) {
+                ret += "<div class=\"slot\">" + course + "</div>\n";
+                coursesCount--;
+            }
+            for (; coursesCount > 0; coursesCount--) {
+                ret += "<div class=\"slot\"></div>\n";
+            }
+            ret += "</div>"
+            return ret;
+        }
+    }
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -31,13 +54,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
+
+var index = require('./routes/index');
+// Example route
+// var user = require('./routes/user');
 
 app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@
 /*
  * GET home page.
  */
+var data = {
+    "user": require('../data/users.json')['Ian Drosos'],
+    "majors": require('../data/majors.json'),
+    "minors": require('../data/minors.json'),
+    "colleges": require('../data/colleges.json'),
+    "courses": require('../data/courses.json')
+}
 
 exports.view = function(req, res){
-  res.render('index');
+    console.log("data keys: " + Object.keys(data));
+    res.render('index', data);
 };
