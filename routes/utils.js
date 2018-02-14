5ac2818fce621426203624a6a2bf392ae650b7df
Michael Liu <miliu@protonmail.com>
2018-02-13 17:20:26 -0800

    whoops, exports should be an object

--- routes/utils.js
+++ routes/utils.js
@@ -1,8 +1,8 @@
-module.exports = (
-    checkReqs = function(courses, reqs) {
+module.exports = {
+    checkReqs: function(courses, reqs) {
         var numReqs = reqs[0];
         for (let req of reqs.slice(1)) {
-
+            console.log(req);
         }
     }
-);
+};
