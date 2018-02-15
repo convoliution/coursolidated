7bd1c0737ff49b3470eef657a1dd03dfabe3df22
Michael Liu <miliu@protonmail.com>
2018-02-15 10:19:45 -0800

    work on checking course card status

--- public/js/draggable.js
+++ public/js/draggable.js
@@ -12,7 +12,28 @@ $(function() {
             if ($(this).children().length < 6) {
                 $(this).removeClass('full');
             }
+        },
+        // check prereqs for all courses on drop
+        deactivate: function(event, ui) {
+            $(this).parents('.schedule').find('.course').map(setCardStatus);
         }
     });
-    
+    $('#toadd-menu .requirement').sortable({
+        connectWith: ".term:not(.full)",
+        tolerance: "pointer",
+        revert: 100
+        // resize card on grab
+    });
+
+    function setCardStatus() {
+                                // courses from prev terms in same year
+        let preceedingCourses = $(this).parent().prevAll('.term').children('.course')
+                                // courses from prev years
+                                .add($(this).parent().parent().prevAll('.year')
+                                .children('.term').children('.course')).get();
+        //console.log(new Set(preceedingCourses));
+
+        // get requirements from courses.json
+        var course = $(this).data('course');
+    }
 });
