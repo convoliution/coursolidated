979fd85cdc822be7200a35a52800ce187fd6a8a6
Dennis Liu <del012@ucsd.edu>
2018-03-04 21:06:50 -0800

    changing button name

--- public/js/index.js
+++ public/js/index.js
@@ -123,7 +123,7 @@ function showCourseInfoDialog(event) {
         $('#course-info').html(html);
         //append a button
         $('#course-info').append("<button class=\"delete\">TRUUU</button>");
-        $('#course-info > button.delete').html("Truuuuu");
+        $('#course-info > button.delete').val("Truuuuu");
         $('#course-info > button.delete').tap(function(event) {
             event.preventDefault();
             // delete the class and send the parent term div to server
